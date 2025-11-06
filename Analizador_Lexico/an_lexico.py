c3fa36addd8d2589d6ce3a637b9146aded63cbb0
JunhaoZhg <junhizhg.9@gmail.com>
2025-11-06 23:06:17 +0100

    Update an_lexico.py

--- Analizador_Lexico/an_lexico.py
+++ Analizador_Lexico/an_lexico.py
@@ -30,6 +30,11 @@ class Lexer:
         self.line = 1
         self.column = 1
         self.pos = 0
+        self.current_function = None
+        self._awaiting_function_body_of = None
+        self._brace_depth = 0
+        self._pending_param_names = set()
+
 
     # ---------------- Tokenización general ----------------
     def tokenize(self):
@@ -104,16 +109,51 @@ class Lexer:
             self._advance()
         lexeme = self.source[start_pos:self.pos]
         ttype = KEYWORDS.get(lexeme, TokenType.ID)
+
         if ttype == TokenType.ID:
-            entry = self.symbols.add_if_absent(lexeme, self.line)
-            token = Token(ttype, lexeme, self.line, start_col, attribute=entry.name)
+            if self.current_function:
+                # 1) ¿Existe ya en la local? (parámetro o let local)
+                ltab = self.symbols.locales.get(self.current_function)
+                lentry = ltab.buscar(lexeme) if ltab else None
+                if lentry:
+                    entry = lentry
+                else:
+                    # 2) ¿Existe en la global? => usar la global, NO crear local
+                    gentry = self.symbols.buscar(lexeme)
+                    if gentry:
+                        entry = gentry
+                    else:
+                        # 3) No existe en ninguna: opcionalmente crear local "id"
+                        self.symbols.add_local(self.current_function, lexeme, "id")
+                        entry = self.symbols.locales[self.current_function].buscar(lexeme)
+
+            elif self._awaiting_function_body_of and lexeme in self._pending_param_names:
+                # En cabecera: es parámetro ya registrado en local; no tocar global
+                fname = self._awaiting_function_body_of
+                entry = self.symbols.locales[fname].buscar(lexeme)
+
+            else:
+                # Ámbito global normal
+                if hasattr(self.symbols, "locales") and lexeme in self.symbols.locales:
+                    entry = (self.symbols.buscar(lexeme)
+                            or self.symbols.add_if_absent(lexeme, self.line, "void", categoria="Función"))
+                else:
+                    entry = self.symbols.add_if_absent(lexeme, self.line)
+
+            token = Token(ttype, lexeme, self.line, start_col, attribute=getattr(entry, "name", lexeme))
         else:
             token = Token(ttype, lexeme, self.line, start_col)
+
         self.tokens.append(token)
         self.column += len(lexeme)
 
         if ttype == TokenType.LET:
             self._propagate_type_declaration()
+        elif ttype == TokenType.FUNCTION:
+            self._propagate_function_declaration()
+
+
+
 
     # --- números ---
     def _number(self, first_char):
@@ -158,14 +198,35 @@ class Lexer:
     # --- operadores / delimitadores ---
     def _operator_or_punctuator(self, c):
         start_col = self.column
-        # operadores de dos caracteres (|=)
+
         if c == "|" and self._peek() == "=":
             self._advance()
             self.tokens.append(Token(TokenType.OR_ASSIGN, "|=", self.line, start_col))
             self.column += 2
             return
 
-        # operadores simples
+        if c == "{":
+            if self._awaiting_function_body_of and not self.current_function:
+                self.current_function = self._awaiting_function_body_of
+                self._awaiting_function_body_of = None
+                self._brace_depth = 1
+                self._pending_param_names.clear()  # << limpiar lista de parámetros ya activados
+            elif self.current_function:
+                self._brace_depth += 1
+            self.tokens.append(Token(TokenType.LBRACE, "{", self.line, start_col))
+            self.column += 1
+            return
+
+        if c == "}":
+            if self.current_function:
+                self._brace_depth -= 1
+                if self._brace_depth <= 0:
+                    self.current_function = None
+                    self._brace_depth = 0
+            self.tokens.append(Token(TokenType.RBRACE, "}", self.line, start_col))
+            self.column += 1
+            return
+
         single_ops = {
             "+": TokenType.PLUS,
             "<": TokenType.LT,
@@ -173,17 +234,16 @@ class Lexer:
             "=": TokenType.ASSIGN,
             "(": TokenType.LPAREN,
             ")": TokenType.RPAREN,
-            "{": TokenType.LBRACE,
-            "}": TokenType.RBRACE,
             ";": TokenType.SEMICOLON,
             ",": TokenType.COMMA,
         }
-
         if c in single_ops:
             self.tokens.append(Token(single_ops[c], c, self.line, start_col))
             self.column += 1
         else:
             self._add_error(f"Carácter no reconocido: '{c}'", start_col)
+
+
 
     # --- errores ---
     def _add_error(self, message: str, column: int):
@@ -192,39 +252,38 @@ class Lexer:
 
     # ------detectar tipo en declaraciones "let <tipo> <id>;"-------
     def _propagate_type_declaration(self):
-        """Detecta 'let <tipo> <id>' y asigna el tipo al símbolo."""
         temp_pos = self.pos
         temp_line = self.line
         temp_col = self.column
 
-        # Saltar espacios
         while self._peek() in " \t\r\n":
             if self._peek() == "\n":
-                temp_line += 1
-                temp_col = 1
+                temp_line += 1; temp_col = 1
             else:
                 temp_col += 1
             self.pos += 1
 
-        # Leer tipo
+        # tipo
         start_tipo = self.pos
         while self._peek().isalpha():
             self._advance()
         tipo = self.source[start_tipo:self.pos]
 
-        # Saltar espacios
         while self._peek() in " \t":
             self._advance()
 
-        # Leer identificador
+        # id
         start_id = self.pos
         while self._peek().isalnum() or self._peek() == "_":
             self._advance()
         identificador = self.source[start_id:self.pos]
 
         if tipo and identificador:
-            self.symbols.add_if_absent(identificador, temp_line)
-            self.symbols.update_type(identificador, tipo)
+            if self.current_function:
+                self.symbols.add_local(self.current_function, identificador, tipo)
+            else:
+                self.symbols.add_if_absent(identificador, temp_line, tipo)
+                self.symbols.update_type(identificador, tipo)
 
         self.pos = temp_pos
 
@@ -256,3 +315,109 @@ class Lexer:
 
         # Restaurar posición del lexer
         self.pos = temp_pos
+
+    def _propagate_function_declaration(self):
+
+        temp_pos = self.pos
+        temp_line = self.line
+        temp_col = self.column
+
+        # espacios
+        while self._peek() in " \t\r\n":
+            if self._peek() == "\n":
+                temp_line += 1; temp_col = 1
+            else:
+                temp_col += 1
+            self.pos += 1
+
+        # tipo de retorno
+        start_tipo = self.pos
+        while self._peek().isalpha():
+            self._advance()
+        ret_type = self.source[start_tipo:self.pos].strip()
+        if not ret_type:
+            self.pos = temp_pos
+            return
+
+        # espacios
+        while self._peek() in " \t\r\n":
+            self.pos += 1
+
+        # nombre de función
+        if not (self._peek().isalpha() or self._peek() == "_"):
+            self.pos = temp_pos
+            return
+        start_name = self.pos
+        while self._peek().isalnum() or self._peek() == "_":
+            self._advance()
+        func_name = self.source[start_name:self.pos]
+
+        # espacios
+        while self._peek() in " \t\r\n":
+            self.pos += 1
+
+        # '('
+        if self._peek() != "(":
+            self.pos = temp_pos
+            return
+        self._advance()
+
+        # parámetros
+        param_types = []
+        param_names = []
+        while not self._is_at_end():
+            while self._peek() in " \t\r\n":
+                self.pos += 1
+            if self._peek() == ")":
+                self._advance()
+                break
+
+            # tipo del parámetro
+            start_ptype = self.pos
+            while self._peek().isalpha():
+                self._advance()
+            ptype = self.source[start_ptype:self.pos].strip()
+            if not ptype:
+                break
+
+            while self._peek() in " \t":
+                self._advance()
+
+            # nombre del parámetro
+            if not (self._peek().isalpha() or self._peek() == "_"):
+                break
+            start_pid = self.pos
+            while self._peek().isalnum() or self._peek() == "_":
+                self._advance()
+            pid = self.source[start_pid:self.pos]
+
+            # registrar en local
+            self.symbols.add_local(func_name, pid, ptype)
+            param_types.append(ptype)
+            param_names.append(pid)
+
+            while self._peek() in " \t\r\n":
+                self.pos += 1
+            if self._peek() == ",":
+                self._advance()
+                continue
+            elif self._peek() == ")":
+                self._advance()
+                break
+            else:
+                break
+
+        # registrar función con metadatos y preparar apertura de cuerpo
+        try:
+            self.symbols.declare_function(func_name, ret_type or "void", param_types)
+        except AttributeError:
+            self.pos = temp_pos
+            return
+
+        self._awaiting_function_body_of = func_name
+        self._pending_param_names = set(param_names)
+
+        # restaurar posición
+        self.pos = temp_pos
+
+
