e61e439a9eaa193acb3c1e1fea643879560745ad
JunhaoZhg <junhizhg.9@gmail.com>
2026-02-20 21:35:32 +0100

    Refactor GestorTablas class structure

--- Tabla_Simbolos/gestor_tabla.py
+++ Tabla_Simbolos/gestor_tabla.py
@@ -2,7 +2,7 @@ from Tabla_Simbolos.symbol_table import SymbolTable
 from Tabla_Simbolos.symbol_entry import SymbolEntry
 
 class GestorTablas:
-    """Gestiona la tabla global y las tablas locales por función."""
+    
 
     def __init__(self):
         self.tabla_global = SymbolTable("global")
@@ -13,7 +13,7 @@ class GestorTablas:
         self.tabla_global.add_if_absent(lexema, 0, tipo, categoria="Variable")
 
     def registrar_funcion(self, nombre_funcion, tipo="void"):
-        """Crea la entrada de la función en global y su tabla local."""
+       
         self.tabla_global.add_if_absent(nombre_funcion, 0, tipo, categoria="Función")
         if nombre_funcion not in self.locales:
             self.locales[nombre_funcion] = SymbolTable(nombre_funcion)
@@ -32,3 +32,5 @@ class GestorTablas:
 
     def dump(self):
         return str(self)
+
+
