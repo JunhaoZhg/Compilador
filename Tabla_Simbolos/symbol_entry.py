bb59d32d5492cd9f4ef18b544102c702a735c3a8
JunhaoZhg <junhizhg.9@gmail.com>
2026-02-20 21:36:04 +0100

    Update class
    Added a class method to reset the index counter for symbol entries and updated string representations to include the index.

--- Tabla_Simbolos/symbol_entry.py
+++ Tabla_Simbolos/symbol_entry.py
@@ -1,5 +1,17 @@
 class SymbolEntry:
+    _contador_indices = 0
+
+    @classmethod
+    def reset_contador(cls):
+        
+        cls._contador_indices = 0
+    # ------------------------------------------------------------------------
+
     def __init__(self, categoria, lexema, tipo="-", desplazamiento=None, valor=None, num_params=0, tipos_params=None):
+        self.index = SymbolEntry._contador_indices
+        SymbolEntry._contador_indices += 1
+        # --------------------------------------------------
+
         self.categoria = categoria
         self.lexema = lexema
         self.tipo = tipo
@@ -12,10 +24,13 @@ class SymbolEntry:
         return self.lexema
 
     def __str__(self):
+        prefix = f"({self.index})" 
+        # ----------------------------------------------------------
+
         if self.categoria == "Función":
             tipos_str = "[" + ", ".join(self.tipos_params) + "]"
-            return f"[{self.categoria}] lexema='{self.lexema}', tipo='{self.tipo}', Nºparams={self.num_params}, tipos={tipos_str}"
+            return f"{prefix} [{self.categoria}] lexema='{self.lexema}', tipo='{self.tipo}', Nºparams={self.num_params}, tipos={tipos_str}"
         else:
             tipo = self.tipo or "-"
             off = self.desplazamiento if self.desplazamiento is not None else "-"
-            return f"[{self.categoria}] lexema='{self.lexema}', tipo='{tipo}', desplazamiento={off}"
+            return f"{prefix} [{self.categoria}] lexema='{self.lexema}', tipo='{tipo}', desplazamiento={off}"
